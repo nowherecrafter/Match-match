a7d9e527fae1f78bbdbbcf29ea735ce56b43459d
nowherecrafter <110341990+nowherecrafter@users.noreply.github.com>
2025-05-24 17:11:54 +0200

    Router moved to modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { header } from './components/header';
-import { Router } from './components/router';
+import { Router } from './modules/router';
 import { AppState } from './services/state';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as bootstrap from 'bootstrap';  
